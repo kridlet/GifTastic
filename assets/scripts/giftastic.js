d74db86b0a86f2628d2fe0f362e9043115a7c92e
dirk wiggins <dirk@echo19.com>
2018-03-12 11:08:56 -0400

    javascript, html, and css additions

--- assets/scripts/giftastic.js
+++ assets/scripts/giftastic.js
@@ -1,13 +1,20 @@
 // Function for displaying topic buttons
 function renderButtons() {
     $("#buttons-view").empty();
+    var buttonDisplay = '';
     for (i = 0; i < topics.length; i++) {
-        $("#buttons-view").append("<button class='topic-button' data-topic='" + topics[i] + "'>" + topics[i] + "</button>");
+        buttonDisplay += '<button type="button" class="btn btn-secondary btn-sm topic-button" data-topic="' + topics[i] + '">' + camelize(topics[i]) + '</button>';
     }
+    $("#buttons-view").append(buttonDisplay);
+}
+
+function camelize(str) {
+    return str.split(/\s+|-|_/).map(function (word, index) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join(' ');
 }
 
 function fetchGiphys(button) {
-    console.log(button);
     buttonTopic = $(button).attr("data-topic");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonTopic +
         "&api_key=KNIJIp9YyOrkUXM2mOjvZRtpKVDflwVM&limit=10";
@@ -17,21 +24,25 @@ function fetchGiphys(button) {
         method: "GET"
     }).then(function (response) {
         var results = response.data;
-        console.log(results);
+        cardGroupHTML = '<h3>' + camelize(buttonTopic) + '</h3><div class="card-deck">';
+        var topicImage = "";
         for (var i = 0; i < results.length; i++) {
-            topicDiv = $("<div>");
-            topicImage = $("<img>");
-            $(topicImage).attr('src', results[i].images.fixed_height_still.url);
-            $(topicImage).attr('data-still', results[i].images.fixed_height_still.url);
-            $(topicImage).attr('data-animate', results[i].images.fixed_height.url);
-            $(topicImage).attr('data-state', 'still');
-            $(topicImage).attr('class', 'gif');
-
-            $(topicDiv).append(topicImage);
-            $(topicDiv).append('<p>' + results[i].rating + '</p>');
-            $("#gifs-appear-here").prepend(topicDiv);
+            topicImage += "<div class='card mb-4'><img src='" + results[i].images.fixed_height_still.url + "' data-still='" + results[i].images.fixed_height_still.url + "' data-animate='" + results[i].images.fixed_height.url + "' data-state='still' class='card-img-top img-fluid gif' alt='" + results[i].title + "'>";
+            topicImage += "<div class='card-body'><h4 class='card-title'>" + results[i].title + "</h4></div>";
+            topicImage += "<div class='card-footer'><small class='text-muted'>Rating: " + results[i].rating.toUpperCase() + "</small>";
+            topicImage += '<a href="' + results[i].images.original.url + '" download="' + results[i].title + '"><button class="btn btn-outline-secondary btn-sm download-button">Download</button></a></div></div>';
+            if ((i + 1) % 2 === 0) {
+                topicImage += "<div class='w-100 d-none d-sm-block d-md-block d-lg-block d-xl-block'><!-- wrap every 2 on sm--></div>";
+            }
         }
+        cardGroupHTML += topicImage + "</div>";
+        $("#dynamic-gif").prepend(cardGroupHTML);
     });
+}
+
+function fetchFavorites() {
+    favoriteGroup = '<h3>Favorites</h3>';
+    $("#dynamic-favorite").html(favoriteGroup);
 }
 
 function toggleAnimation(gif) {
@@ -47,30 +58,30 @@ function toggleAnimation(gif) {
 
 function addTopic() {
     event.preventDefault();
-    // This line grabs the input from the textbox
-    var topic = $("#topic-input").val().trim();
-    if (!topics.includes(topic)) {
-        // Adding topic from the textbox to our array
-        topics.push(topic);
-        // Calling renderButtons which handles the processing of our topic array
-        renderButtons();
+    if ($("#topic-input").val() != '') {
+        // This line grabs the input from the textbox
+        var topic = $("#topic-input").val().trim();
+        if (!topics.includes(topic)) {
+            // Adding topic from the textbox to our array
+            topics.push(topic);
+            // Calling renderButtons which handles the processing of our topic array
+            renderButtons();
+        }
     }
 }
 
-var topics = ['cats', 'dogs', 'birds'];
+var topics = ['shrimp-kabobs', 'shrimp creole', 'shrimp gumbo', 'pan fried shrimp', 'deep fried shrimp', 'stir-fried shrimp', 'pineapple shrimp', 'lemon shrimp', 'coconut shrimp', 'pepper shrimp', 'shrimp soup', 'shrimp stew', 'shrimp salad', 'shrimp and potatoes', 'shrimp burger', 'shrimp sandwich'];
 
 $(document).ready(function () {
     renderButtons();
-    $(document).on('click','#add-topic', function () {
-        console.log('add topic click - add topic');
+    fetchFavorites();
+    $(document).on('click', '#add-topic', function () {
         addTopic();
     });
-    $(document).on('click','.topic-button', function () {
-        console.log('topic click - fetch giphy');
+    $(document).on('click', '.topic-button', function () {
         fetchGiphys(this);
     });
-    $(document).on('click','.gif', function () {
-        console.log('gif click - toggle animation');
+    $(document).on('click', '.gif', function () {
         toggleAnimation(this);
     });
 });
